e9a0d1ccd5e962927d6a5ec5068f784ad13935af
AbelSalazar2024 <157082464+AbelSalazar2024@users.noreply.github.com>
2024-02-23 01:24:41 +0000

    HW4

--- src/homework/e_functions/main.py
+++ src/homework/e_functions/main.py
@@ -1,1 +1,12 @@
-#
+#H4 main program
+
+import value_return
+
+epoch_seconds = 3800
+
+hours = value_return.get_hour(epoch_seconds)
+minutes = value_return.get_minutes(epoch_seconds)
+seconds = value_return.get_seconds(epoch_seconds)
+
+show_time = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+print("The time is:", show_time)
