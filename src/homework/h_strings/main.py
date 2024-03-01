5a53073f655999fa3ee8bf25e5ef8a366b4179de
AbelSalazar2024 <157082464+AbelSalazar2024@users.noreply.github.com>
2024-03-01 04:43:32 +0000

    HW5

--- src/homework/h_strings/main.py
+++ src/homework/h_strings/main.py
@@ -1,1 +1,43 @@
-#
+#HW5 main program
+import strings
+
+while True:
+    print("MENU")
+    print("1 - Hamming Distance")
+    print("2 - DNA Complement")
+    print("3 - Exit")
+
+    select = input ("Enter a number 1-3: ")
+
+    if select == '1':
+        while True:
+            dna1 = input("Enter first DNA string: ")
+            dna2 = input("Enter second DNA string: ")
+        
+            if dna1.isalpha() and dna2.isalpha():
+                break
+            else:
+                print("Invalid input. Enter valid DNA strings.")
+
+        result = strings.get_hamming_distance(dna1, dna2)
+        print(f"The Hamming Distance is: {result}")
+
+    elif select == '2':
+        while True:
+            dna = input("Enter a DNA string: ")
+
+            if dna.isalpha():
+                break
+            else:
+                print("Invalid input. Enter a valid DNA string.")    
+            
+        result = strings.get_dna_complement(dna)
+        print(f"The complementary DNA string is: {result}")
+
+    elif select == '3':
+        print("Exiting the program")
+        break
+
+    else:
+        print("Invalid selection. Please enter a number 1-3.")
+        
--- src/homework/h_strings/strings.py
+++ src/homework/h_strings/strings.py
@@ -1,1 +1,21 @@
-#
+#HW5
+
+def get_hamming_distance(dna1, dna2):
+    
+    if len(dna1) != len(dna2):
+        return None
+    
+    distance = 0
+
+    for i in range(len(dna1)):
+        if dna1[i] != dna2[i]:
+            distance += 1
+
+    return distance
+
+def get_dna_complement(dna):
+    
+    complement_dict = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    complemented_dna = ''.join(complement_dict.get(char, char) for char in reversed(dna))
+    
+    return complemented_dna
