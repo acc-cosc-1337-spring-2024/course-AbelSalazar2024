29a74f6d3544dc73bbb7ea801baf2e6d3716887d
AbelSalazar2024 <157082464+AbelSalazar2024@users.noreply.github.com>
2024-03-29 03:07:39 +0000

    HW6 - dictionaries

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,1 +1,30 @@
-#
+#HW6 main program
+
+import dictionary
+
+inventory = {}
+while True:
+    
+    print("1 - Add or Update Item")
+    print("2 - Delete Item")
+    print("3 - Exit")
+
+    select = input("Enter a number 1-3: ")
+
+    if select == "1":
+        widget_name = input("Enter the widget name: ")
+        quantity = int(input("Enter the quantity: "))
+        dictionary.add_inventory(inventory, widget_name, quantity)            
+        print(f"{quantity} {widget_name}(s) added/updated in inventory.")
+    
+    elif select == "2":
+        widget_name = input("Enter the widget name to delete: ")
+        result = dictionary.remove_inventory_widget(inventory, widget_name)
+        print(result)
+    
+    elif select == "3":
+        print("Exiting the program. ")
+        break
+
+    else:
+        print("Invalid selection. Please select 1-3: ")
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -1,1 +1,26 @@
-#
+#HW6 unittest
+
+import unittest
+
+from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory_widget
+
+class Test_Config(unittest.TestCase):
+
+    def test_add_inventory(self):
+        inventory = {}
+        
+        add_inventory(inventory, "Widget1", 10)
+        self.assertEqual(inventory["Widget1"], 10)
+        add_inventory(inventory, "Widget1", 25)
+        self.assertEqual(inventory["Widget1"], 35)
+        add_inventory(inventory, "Widget1", -10)
+        self.assertEqual(inventory["Widget1"], 25)
+
+    def test_remove_inventory_widget(self):
+        inventory = {}
+        
+        add_inventory(inventory, "Widget1", 10)
+        add_inventory(inventory, "Widget2", 25)
+        remove_inventory_widget(inventory, "Widget1")
+        self.assertEqual(inventory.__len__(), 1)
+        self.assertEqual(inventory["Widget2"], 25)
