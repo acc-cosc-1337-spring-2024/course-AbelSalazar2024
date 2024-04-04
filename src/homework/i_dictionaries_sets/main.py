02007486f4a24c0115c6d5defc57095a967a7993
AbelSalazar2024 <157082464+AbelSalazar2024@users.noreply.github.com>
2024-04-04 02:38:25 +0000

    Hw7

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,7 +1,35 @@
-#HW6 main program
+#HW7 main program
 
-import dictionary
+import sets
 
+prog1 = {'Student1', 'Student2', 'Student3'}
+prog2 = {'Student3', 'Student4', 'Student5'}
+
+while True:
+    print("1-Students who took prog1 and prog2")
+    print("2-Students who took prog2 only")
+    print("3-Students who took prog1 not prog2")
+    print("4-Students who took prog2 not prog1")
+    print("5-Exit")
+
+    select = input("Enter your selection: ")
+
+    if select == '1':
+        print("Students who took prog1 and prog2:", sets.get_students_who_took_prog1_and_prog2(prog1, prog2))
+    elif select == '2':
+        print("Students who took prog2 only:", sets.get_students_who_took_prog2_only(prog1, prog2))
+    elif select == '3':
+        print("Students who took prog1 not prog2:", sets.get_students_who_took_prog1_not_prog_2(prog1, prog2))
+    elif select == '4':
+        print("Students who took prog2 not prog1:", sets.get_students_who_took_prog2_not_prog_1(prog1, prog2))
+    elif select == '5':
+        print("Exiting the program.")
+        break
+    else:
+        print("Invalid selection. Please enter a number between 1-5.")
+
+"""
+HW6 - main program
 inventory = {}
 while True:
     
@@ -28,3 +56,4 @@ while True:
 
     else:
         print("Invalid selection. Please select 1-3: ")
+"""
--- tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
+++ tests/homework/i_dictionaries_sets/tests_dictionaries_and_sets.py
@@ -2,25 +2,30 @@
 
 import unittest
 
-from src.homework.i_dictionaries_sets.dictionary import add_inventory, remove_inventory_widget
+from src.homework.i_dictionaries_sets.sets import get_students_who_took_prog1_and_prog2, get_students_who_took_prog1_not_prog_2, get_students_who_took_prog2_not_prog_1, get_students_who_took_prog2_only
 
 class Test_Config(unittest.TestCase):
 
-    def test_add_inventory(self):
-        inventory = {}
-        
-        add_inventory(inventory, "Widget1", 10)
-        self.assertEqual(inventory["Widget1"], 10)
-        add_inventory(inventory, "Widget1", 25)
-        self.assertEqual(inventory["Widget1"], 35)
-        add_inventory(inventory, "Widget1", -10)
-        self.assertEqual(inventory["Widget1"], 25)
+    def test_get_students_who_took_prog1_and_prog2(self):
+        prog1 = {'Student1', 'Student2', 'Student3'}
+        prog2 = {'Student3', 'Student4', 'Student5'}
+        expected_result = {'Student3'}
+        self.assertEqual(get_students_who_took_prog1_and_prog2(prog1, prog2), expected_result)
 
-    def test_remove_inventory_widget(self):
-        inventory = {}
-        
-        add_inventory(inventory, "Widget1", 10)
-        add_inventory(inventory, "Widget2", 25)
-        remove_inventory_widget(inventory, "Widget1")
-        self.assertEqual(inventory.__len__(), 1)
-        self.assertEqual(inventory["Widget2"], 25)
+    def test_get_students_who_took_prog2_only(self):
+        prog1 = {'Student1', 'Student2', 'Student3'}
+        prog2 = {'Student3', 'Student4', 'Student5'}
+        expected_result = {'Student4', 'Student5'}
+        self.assertEqual(get_students_who_took_prog2_only(prog1, prog2), expected_result)
+
+    def test_get_students_who_took_prog1_not_prog_2(self):
+        prog1 = {'Student1', 'Student2', 'Student3'}
+        prog2 = {'Student3', 'Student4', 'Student5'}
+        expected_result = {'Student1', 'Student2'}
+        self.assertEqual(get_students_who_took_prog1_not_prog_2(prog1, prog2), expected_result)
+
+    def test_get_students_who_took_prog2_not_prog_1(self):
+        prog1 = {'Student1', 'Student2', 'Student3'}
+        prog2 = {'Student3', 'Student4', 'Student5'}
+        expected_result = {'Student4', 'Student5'}
+        self.assertEqual(get_students_who_took_prog2_not_prog_1(prog1, prog2), expected_result)
